0678b13d62b3117422fc03c2fd93c9d8b5b6cc34
Andisyc <Andisyc@163.com>
2026-01-07 17:08:02 +0800

    update commit

--- robojudo/config/g1/g1_cfg.py
+++ robojudo/config/g1/g1_cfg.py
@@ -1,26 +1,30 @@
 from robojudo.config import cfg_registry
+
 from robojudo.controller.ctrl_cfgs import (
     JoystickCtrlCfg,  # noqa: F401
     KeyboardCtrlCfg,  # noqa: F401
-    UnitreeCtrlCfg,  # noqa: F401
-)
+    UnitreeCtrlCfg,)  # noqa: F401
+
 from robojudo.pipeline.pipeline_cfgs import (
     RlLocoMimicPipelineCfg,  # noqa: F401
     RlMultiPolicyPipelineCfg,  # noqa: F401
-    RlPipelineCfg,  # noqa: F401
-)
+    RlPipelineCfg,)  # noqa: F401
 
+# ======================== Ctrl Configs ======================== #
 from .ctrl.g1_beyondmimic_ctrl_cfg import G1BeyondmimicCtrlCfg  # noqa: F401
 from .ctrl.g1_motion_ctrl_cfg import (  # noqa: F401
     G1MotionCtrlCfg,
     G1MotionH2HCtrlCfg,
     G1MotionKungfuBotCtrlCfg,
-    G1MotionTwistCtrlCfg,
-)
+    G1MotionTwistCtrlCfg,)
 from .ctrl.g1_twist_redis_ctrl_cfg import G1TwistRedisCtrlCfg  # noqa: F401
+
+# ======================== Env Configs ======================== #
 from .env.g1_dummy_env_cfg import G1DummyEnvCfg  # noqa: F401
 from .env.g1_mujuco_env_cfg import G1_12MujocoEnvCfg, G1_23MujocoEnvCfg, G1MujocoEnvCfg  # noqa: F401
 from .env.g1_real_env_cfg import G1RealEnvCfg, G1UnitreeCfg  # noqa: F401
+
+# ======================== Policy Configs ======================== #
 from .policy.g1_amo_policy_cfg import G1AmoPolicyCfg  # noqa: F401
 from .policy.g1_asap_policy_cfg import G1AsapLocoPolicyCfg, G1AsapPolicyCfg  # noqa: F401
 from .policy.g1_beyondmimic_policy_cfg import G1BeyondMimicPolicyCfg  # noqa: F401
